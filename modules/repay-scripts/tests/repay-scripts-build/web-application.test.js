cd6d5de5344c54af3efea092f93758af3553193d
Dhalton <dhuber@repay.com>
2020-06-26 11:33:34 -0700

    test(repay-scripts): add tests for custom index.html templates
    CACTUS-253

--- modules/repay-scripts/tests/repay-scripts-build/web-application.test.js
+++ modules/repay-scripts/tests/repay-scripts-build/web-application.test.js
@@ -14,4 +14,16 @@ describe('@repay/repay-scripts', () => {
     const content = await page.$eval('.app-root', (el) => el.outerHTML)
     expect(content).toMatchSnapshot()
   })
+
+  it('should build a web application using a custom index.html', async () => {
+    const test = new TestSetup('web-application')
+    await test.setup()
+    await test.exec('yarn repay-scripts build --html-template index.html --babel-env test index.js')
+
+    test.startServer()
+    const page = await test.getPage()
+    await page.goto('http://localhost:8100')
+    const content = await page.$eval("head > meta[name='viewport']", (el) => el.content)
+    expect(content).toBe('test')
+  })
 })
--- modules/repay-scripts/tests/repay-scripts-dev/web-application.test.js
+++ modules/repay-scripts/tests/repay-scripts-dev/web-application.test.js
@@ -22,4 +22,25 @@ describe('@repay/repay-scripts', () => {
     const content = await page.$eval('.app-root', (el) => el.outerHTML)
     expect(content).toMatchSnapshot()
   })
+
+  it('should build a web application for local development using a custom index.html', async () => {
+    const test = new TestSetup('web-application')
+    await test.setup()
+    test.spawn('yarn', [
+      'repay-scripts',
+      'dev',
+      '--babel-env',
+      'production',
+      'index.js',
+      '--port',
+      '9696',
+      '-t',
+      'index.html',
+    ])
+    await test.waitForText('Compiled successfully')
+    const page = await test.getPage()
+    await page.goto('https://localhost:9696')
+    const content = await page.$eval("head > meta[name='viewport']", (el) => el.content)
+    expect(content).toBe('test')
+  })
 })
